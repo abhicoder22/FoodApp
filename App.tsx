32eef7628d181fcbdfc67d35b47eb4e0f097962e
abhicoder22 <abhishekpal.ap98@gmail.com>
2023-03-06 18:34:18 +0530

    login layout

--- App.tsx
+++ App.tsx
@@ -10,7 +10,11 @@ const MyApp = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{headerShown: false}}
+        />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -5,25 +5,34 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
-  Button,
+  ImageBackground,
+  Image,
 } from 'react-native';
 
 const Login = ({navigation}) => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.titleStyle}>Login</Text>
-      <View style={styles.userStyle}>
-        <TextInput style={styles.inputStyle} placeholder="enter username" />
+      <View>
+        <ImageBackground
+          source={require('../../assets/loginback.png')}
+          style={styles.imageStyle}>
+          <Text style={styles.titleStyle}>LOGIN</Text>
+          <Image source={require('../../assets/logoFront.png')}></Image>
+        </ImageBackground>
       </View>
 
-      <View style={styles.userStyle}>
-        <TextInput style={styles.inputStyle} placeholder="enter password" />
+      <View style={styles.emailStyle}>
+        <TextInput style={styles.inputStyle} placeholder="username" />
+      </View>
+
+      <View style={styles.passStyle}>
+        <TextInput style={styles.inputStyle} placeholder="password" />
       </View>
       <View>
         <TouchableOpacity
           style={styles.buttonStyle}
           onPress={() => navigation.navigate('Home')}>
-          <Text style={styles.buttunTextStyle}>Login</Text>
+          <Text style={styles.buttunTextStyle}>LOGIN</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -32,41 +41,51 @@ const Login = ({navigation}) => {
 
 const styles = StyleSheet.create({
   viewStyle: {
-    flex: 1,
-    justifyContent: 'center',
+    alignItems: 'center',
   },
-  userStyle: {
-    height: 50,
-    margin: 10,
-    borderColor: 'black',
-    borderWidth: 2,
+  emailStyle: {
+    height: 55,
+    width: 390,
+    backgroundColor: 'rgb(211,232,249)',
     borderRadius: 12,
+    marginTop: 80,
+  },
+  passStyle: {
+    height: 55,
+    width: 390,
+    backgroundColor: 'rgb(211,232,249)',
+    borderRadius: 12,
+    marginTop: 20,
   },
   inputStyle: {
     fontSize: 20,
     color: 'black',
-    padding: 10,
-  },
-  titleStyle: {
-    color: 'gray',
-    fontSize: 40,
-    alignSelf: 'center',
-    fontWeight: 'bold',
+    paddingStart: 20,
   },
   buttonStyle: {
-    alignItems: 'center',
-    height: 30,
+    height: 55,
+    width: 390,
+    borderRadius: 12,
+    backgroundColor: 'rgb(238,129,51)',
+    marginTop: 140,
   },
   buttunTextStyle: {
-    borderColor: 'black',
-    borderWidth: 1,
-    backgroundColor: 'gray',
-    height: 50,
-    width: 160,
-    borderRadius: 15,
     fontSize: 30,
     fontWeight: 'bold',
-    textAlign: 'center',
+    alignSelf: 'center',
+    color: 'white',
+    padding: 5,
+  },
+  imageStyle: {
+    height: 365,
+    width: 413,
+  },
+  titleStyle: {
+    color: 'white',
+    alignSelf: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 30,
   },
 });
 
